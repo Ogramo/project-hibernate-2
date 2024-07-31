b8be4d3c5155284e9c964f9089f0c5b0777ff1e3
Ogramo <yaroslaw200019@gmail.com>
2024-07-31 18:22:45 +0300

    Entity finished

---
+++ src/main/java/entity/Language.java
@@ -0,0 +1,22 @@
+package entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(schema = "movie", name = "language")
+public class Language {
+    @Id
+    @Column(name = "language_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Byte id;
+
+    @Column(columnDefinition = "char")
+    private String name;
+
+    @Column(name = "last_update")
+    @UpdateTimestamp
+    private LocalDateTime lastUpdate;
+}
